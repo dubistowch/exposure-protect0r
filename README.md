548de1afcbd16d32057e9304d05fa9883fb978d2
Dubi Stow <contact@dubi-stow.tw>
2021-07-03 16:25:19 +0800

    refactor: using struct on background

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Exposure Protect0r
 
-A Chrome extension for streamer who don't want theirs IP showing up on screen when browsering.
+A Chrome extension for streamer who don't want theirs informatin (such as IP) showing up on screen when browsering.
+
+## Support Typs of Informatiom
+* IP Address (public)
 
 
 ## Example
@@ -8,3 +11,13 @@ A Chrome extension for streamer who don't want theirs IP showing up on screen wh
 ### CloudFlare
 
 ![](/docs/cloudflare.png)
+
+
+## To Do
+
+* Detail status on Popup panel: number of hidden texts 
+* Support other types of information (such as email)
+* Support turn off manually
+* Support customize information (Avalible soultion: Ad Block)
+* Support customize HTML tags
+* Support customize CSS selector
--- background.js
+++ background.js
@@ -1,49 +1,107 @@
-function maskIp(realIp) {
-  return realIp.split('').map((n, i) => (i && n != '.') ? '*' : n).join('')
-}
 
-function getText() {
-  return document.body.innerText
-}
+const Protect0r = {
 
-function updateRealIp() {
-  (async () => {
-    const resp = await fetch('https://ipinfo.io/ip')
-    const realIp = await resp.text()
+  mask: '[Exposure Protect0r]',
+  realIpVendor: 'https://ipinfo.io/ip',
+  interestTags: [
+    'p',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'span', 'div',
+    'ul', 'ol', 'li',
+    'font',
+  ],
+
+  listenerRegisted: false,
+
+  async init() {
+
+    chrome.alarms.create('updateRealIp', {
+      when: 1000,
+      periodInMinutes: 1
+    })
+
+    chrome.alarms.onAlarm.addListener(({ name }) => {
+      Protect0r[name].call(Protect0r[name])
+    })
+
+    chrome.webNavigation.onBeforeNavigate.addListener(() => {
+      if (!Protect0r.listenerRegisted) {
+        Protect0r.registerListener()
+        console.log('listenerRegisted')
+      }
+    },
+      {
+        url: [
+          { urlMatches: 'http://*/*' },
+          { urlMatches: 'https://*/*' }
+        ],
+      }
+    )
+
     chrome.storage.local.set({
-      'realIp': realIp
+      interestTags: Protect0r.interestTags,
+      mask: Protect0r.mask
     })
-    const maskedIp = maskIp(realIp)
-    console.log('realIp', maskedIp)
-  })()
-}
+  },
 
-function hideMyIp() {
-  chrome.storage.local.get(['realIp'], ({ realIp }) => {    
-    const getHTML = () => document.body.innerHTML
-      const regexRealIp = new RegExp(realIp, "g")
-      document.body.innerHTML = getHTML().replace(regexRealIp, "[Exposure Protect0r]")
-  })
-}
+  registerListener() {
+    chrome.tabs.onUpdated.addListener(Protect0r.onUpdateListener)
+    chrome.runtime.onMessage.addListener(Protect0r.onMessage)
+    chrome.tabs.onRemoved.addListener(Protect0r.unregisterListener)
+    Protect0r.listenerRegisted = true
+  },
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete' || changeInfo.status == 'loading') {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: hideMyIp,
+  unregisterListener() {
+    chrome.tabs.onUpdated.removeListener(Protect0r.onUpdateListener)
+    chrome.runtime.onMessage.removeListener(Protect0r.onMessage)
+  },
+
+  onUpdateListener(tabId, changeInfo) {
+    if (changeInfo.status == 'complete' || changeInfo.status == 'loading') {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: Protect0r.executeHideMyIp,
+      })
+    }
+  },
+
+  maskIp(realIp) {
+    return realIp.split('').map((n, i) => (i && n != '.') ? '*' : n).join('')
+  },
+
+  updateRealIp() {
+    (async () => {
+      const resp = await fetch(Protect0r.realIpVendor)
+      const realIp = await resp.text()
+      chrome.storage.local.set({
+        'realIp': realIp
+      })
+      const maskedIp = Protect0r.maskIp(realIp)
+      console.log('realIp', maskedIp)
+    })()
+  },
+
+  executeHideMyIp: () => {
+    const replaceInDom = (dom, needle, mask) => {
+      dom.childNodes.forEach((...[, i]) => {
+        if (dom.childNodes[i].nodeValue) {
+          dom.childNodes[i].nodeValue = dom.childNodes[i].nodeValue.replace(needle, mask)
+        }
+      })
+    }
+    chrome.storage.local.get([
+      'realIp', 'interestTags', 'mask'
+    ], ({ interestTags, mask, realIp }) => {
+      const regexRealIp = new RegExp(realIp, 'g');
+      (async () => {
+        await Promise.all(interestTags.map((tag) => {
+          document.querySelectorAll(tag).forEach((dom) => {
+            replaceInDom(dom, regexRealIp, mask)
+          })
+        }))
+      })()
     })
   }
-})
+}
 
-chrome.alarms.create('updateRealIp', {
-  when: 1000,
-  periodInMinutes: 1
-})
-
-chrome.alarms.onAlarm.addListener(({ name }) => {
-  switch (name) {
-    case "updateRealIp":
-      updateRealIp()
-      break
-  }
-})
+Protect0r.init()
